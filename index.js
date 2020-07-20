86c44baa3299c36bb130a15d6be64f6074648ca7
unknown <kalyanveenam@gmail.com>
2020-07-20 23:03:54 +0530

    test commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let express= require('express');
-const app= express();
-app.use(express.static('frontend'));
-app.get('/',(req,res)=>{
-res.send('index.html');
-})
-app.listen('3001',()=>{
-
-    console.log('App is loistening')
-})
+let express = require("express");
+const app = express();
+app.use(express.static("frontend"));
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+app.listen("3001", () => {
+  console.log("App is listening");
+});
