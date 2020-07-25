979b101d3cf225903eaf3fdd73fb80d6ced18e62
karthik.veenam <karthik.veenam@servicenow.com>
2020-07-25 13:29:54 +0530

    MAINT : Pushed cors

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 let express = require("express");
 let path = require("path");
 let mongoose = require("mongoose");
+var cors = require('cors');
 let fs = require("fs");
 let routeNotFound = require("./app/Middlewares/routeValidation");
 let { config } = require("./app/config/appConfig");
 const { Router } = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.static("frontend"));
 let modelPath = path.join(__dirname, "./app/models");
-fs.readdirSync(modelPath).forEach(function (file) {
-  if (~file.indexOf(".js")) {
-    require(modelPath + "/" + file);
-  }
+fs.readdirSync(modelPath).forEach(function(file) {
+    if (~file.indexOf(".js")) {
+        require(modelPath + "/" + file);
+    }
 });
 let routerPath = path.join(__dirname, "./app/routers");
-fs.readdirSync(routerPath).forEach(function (file) {
-  if (~file.indexOf(".js")) {
-    let route = require(routerPath + "/" + file);
-    route.userRoutes(app);
-  }
+fs.readdirSync(routerPath).forEach(function(file) {
+    if (~file.indexOf(".js")) {
+        let route = require(routerPath + "/" + file);
+        route.userRoutes(app);
+    }
 });
 app.use(routeNotFound.routeNotFound);
 console.log(routerPath);
 app.listen(config.PORT, () => {
-  mongoose.connect(config.mongodb.url, { useMongoClient: true });
-  console.log("App is listening on " + config.PORT);
+    mongoose.connect(config.mongodb.url, { useMongoClient: true });
+    console.log("App is listening on " + config.PORT);
 });
